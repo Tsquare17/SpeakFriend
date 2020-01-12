c62ed8942c234256ba1030e23e784a6e68497183
Trevor Thompson <tsquare1717@gmail.com>
2020-01-12 15:49:41 -0500

    Standardize db location.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Speak Friend and Enter
 
 A simple account / password management system.
+
+Speak Friend secures your information using AES/GCM authenticated encryption in an internally stored sqlite database.
+
+## Building
+
+Speak Friend uses Maven to manage dependencies.
+
+### Installing Dependencies
+* mvn install
+
+### Build executable jar
+* mvn package
--- src/main/kotlin/com/tsquare/speakfriend/database/connection/Conn.kt
+++ src/main/kotlin/com/tsquare/speakfriend/database/connection/Conn.kt
@@ -2,11 +2,17 @@ package com.tsquare.speakfriend.database.connection
 
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
+import java.io.File
 import java.sql.Connection
 
-class Conn {
+class Conn() {
     init {
-        Database.connect("jdbc:sqlite:friend.db?foreign_keys=true", "org.sqlite.JDBC")
+        val dir = File(System.getProperty("user.home").toString() + "/.speakfriend")
+        if(!dir.exists()) {
+            dir.mkdir()
+        }
+        val db = System.getProperty("user.home") + "/.speakfriend/friend.db"
+        Database.connect("jdbc:sqlite:$db?foreign_keys=true", "org.sqlite.JDBC")
         TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
     }
 }
